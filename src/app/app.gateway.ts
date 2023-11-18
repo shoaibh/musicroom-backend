551c12b08c7740c14c72719c91fca0eeb33147f7
Apple <apple@cap-rx.com>
2023-11-19 01:06:30 +0530

    working for other clients

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -14,13 +14,14 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
   private clients: Map<string, string[]> = new Map();
+  private roomOwners: Map<string, string> = new Map();
 
   handleConnection(client: Socket): void {
-    console.log(`Client connected: ${client.id}`);
+    // console.log(`Client connected: ${client.id}`);
   }
 
   handleDisconnect(client: Socket): void {
-    console.log(`Client disconnected: ${client.id}`);
+    // console.log(`Client disconnected: ${client.id}`);
     this.leaveAllRooms(client);
   }
 
@@ -28,7 +29,8 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     const rooms = this.clients.get(client.id) || [];
     rooms.forEach((room) => {
       client.leave(room);
-      console.log(`Client ${client.id} left room: ${room}`);
+      // this.roomOwners.delete(room);
+      // console.log(`Client ${client.id} left room: ${room}`);
     });
     this.clients.delete(client.id);
   }
@@ -38,7 +40,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.leaveAllRooms(client); // Leave existing rooms before joining a new one
 
     client.join(roomId);
-    console.log(`Client ${client.id} joined room: ${roomId}`);
+    // console.log(`Client ${client.id} joined room: ${roomId}`);
 
     // Store the list of rooms the client is in
     const clientRooms = this.clients.get(client.id) || [];
@@ -73,9 +75,9 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('change-song')
   async handleChangeSong(client: Socket, payload: any) {
-    console.log(
-      `Received song in room ${payload.roomId} from client ${client.id} `,
-    );
+    // console.log(
+    //   `Received song in room ${payload.roomId} from client ${client.id} `,
+    // );
 
     // Broadcast the message to all clients in the room
     this.server.to(payload.roomId).emit('receive-change-song', payload);
@@ -83,9 +85,9 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('pause-song')
   async handlePauseSong(client: Socket, payload: any) {
-    console.log(
-      `pause song in room ${payload.roomId} from client ${client.id} `,
-    );
+    // console.log(
+    //   `pause song in room ${payload.roomId} from client ${client.id} `,
+    // );
     // Broadcast the message to all clients in the room
     this.server
       .to(payload.roomId)
@@ -98,11 +100,44 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     console.log(
       `play song in room ${payload.roomId} from client ${client.id} `,
     );
+
+    this.roomOwners.set(payload.roomId, client.id);
+
     // Broadcast the message to all clients in the room
     this.server
       .to(payload.roomId)
       .except(client.id)
       .emit('song-played', payload.clientId);
+  }
+
+  @SubscribeMessage('get-current-timestamp')
+  async getCurrentTimeStamp(client: Socket, payload: any) {
+    console.log(
+      `get cureent timestamp song in room ${payload.roomId} from client ${client.id} `,
+    );
+
+    const socketId = this.roomOwners.get(payload.roomId);
+
+    console.log('roomOwner', { socketId, owners: this.roomOwners });
+    // Broadcast the message to all clients in the room
+    this.server.to(socketId).emit('check-current-timestamp', {
+      ownerSocketId: socketId,
+      memberSocketId: client.id,
+    });
+  }
+
+  @SubscribeMessage('send-current-timestamp')
+  async sendCurrentTimeStamp(client: Socket, payload: any) {
+    console.log(
+      `get send timestamp song in room ${payload.currentTimeStamp} from client ${client.id} `,
+    );
+
+    // Broadcast the message to all clients in the room
+    this.server.to(payload.memberSocketId).emit('receive-current-timestamp', {
+      clientId: client.id,
+      currentTimeStamp: payload.currentTimeStamp,
+      timeStamp: payload.timeStamp,
+    });
   }
 
   @SubscribeMessage('seek-song')
@@ -111,18 +146,18 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
       `seek song in room ${payload.roomId} from client ${client.id} to ${payload.seekTime}`,
     );
 
+    this.roomOwners.set(payload.roomId, client.id);
+
+    console.log('==set Room Owner');
     // Include a timestamp when emitting the "seek-song" event
     const timestamp = Date.now();
 
     // Broadcast the seek event to all clients in the room with the timestamp
-    this.server
-      .to(payload.roomId)
-      .except(client.id)
-      .emit('song-seeked', {
-        clientId: payload.clientId,
-        seekTime: payload.seekTime,
-        timestamp,
-      });
+    this.server.to(payload.roomId).except(client.id).emit('song-seeked', {
+      clientId: payload.clientId,
+      seekTime: payload.seekTime,
+      timestamp,
+    });
   }
 
   @SubscribeMessage('refresh-rooms')
